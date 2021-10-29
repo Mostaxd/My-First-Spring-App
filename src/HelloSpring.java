0fc13f1fe01b974c537620f8545d509d823ce3f8
Mostafa Ahmed <53091141+Mostaxd@users.noreply.github.com>
2021-10-28 18:31:57 -0700

    Simple Spring App. App is made configurable without changing the source code.

--- src/HelloSpring.java
+++ src/HelloSpring.java
@@ -1,13 +1,25 @@
-
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class HelloSpring {
-/*
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		//Load Spring configuration file
 		ClassPathXmlApplicationContext context =
-				new ClassPathXmlApplicationContext("AppletContext.xml");
+				new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		// Retrieve Bean from Spring Container
+		Car car = context.getBean("myCar", Car.class);
+		
+		// Call methods on the bean
+		System.out.println(car.checkCar());
+		
+		
 
 	}
-*/
+	
+	//Conclusion: we can control the  type of object using the XML file, without changing the
+	//source code.
+
 }
